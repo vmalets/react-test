2f79bef102daf93420d11787175d327ae21a68b1
vmalets <49402302+vmalets@users.noreply.github.com>
2020-01-20 17:09:42 +0200

    Function removeTodo & AddTodo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
 
 function App() {
   const [todos, setTodos] = React.useState([
     {id: 1, completed: false, title: 'Купить хлеб'},
-    {id: 2, completed: false, title: 'Купить масло'},
+    {id: 2, completed: true, title: 'Купить масло'},
     {id: 3, completed: false, title: 'Купить молоко'},
   ]);
 
@@ -17,11 +19,28 @@ function App() {
     }))
   }
 
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([{
+        title,
+        id: Date.now(),
+        completed: false
+      }])
+    )
+  }
+
   return (
-    <div className='wrapper'>
-      <h1>React tutorial</h1>
-      <TodoList todos={todos} onToggle={toggleTodo}/>
-    </div>
+    <Context.Provider value={{removeTodo}}>
+      <div className='wrapper'>
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo}/>
+        {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo}/>) : (<p>No todos!</p>)}
+      </div>
+    </Context.Provider>
   );
 }
 
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useContext} from "react";
 import PropTypes from "prop-types"
+import Context from "../context";
 
 const styles = {
   li: {
@@ -17,16 +18,22 @@ const styles = {
 };
 
 function TodoItem({todo, index, onChange}) {
-  console.log('todo', todo);
+  const {removeTodo} = useContext(Context);
+  const classes = [];
+
+  if (todo.completed) {
+    classes.push('done')
+  }
+
   return (
     <li style={styles.li}>
-    <span>
-      <input type="checkbox" style={styles.input} onChange={() => onChange(todo.id)}/>
+    <span className={classes.join(' ')}>
+      <input type="checkbox" checked={todo.completed} style={styles.input} onChange={() => onChange(todo.id)}/>
       <strong>{index + 1}</strong>
       &nbsp;
       {todo.title}
     </span>
-      <button className={'rm'}>&times;</button>
+      <button className='rm' onClick={removeTodo.bind(null, todo.id)}>&times;</button>
     </li>
   )
 }
